a4e97cc6e219c868a82df69f4595255986fc364d
2345 <youngjun8947@gmail.com>
2023-11-29 11:02:46 +0900

    2023.11.29

--- StudyMatch/src/main/java/test/Main.java
+++ StudyMatch/src/main/java/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("안녕하시와여ㅎ");
+		System.out.println("안녕하세요. 더 잘 합시다.");
 	}
 
 }
