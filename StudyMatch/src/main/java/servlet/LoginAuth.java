65ffab4191ffbd9c98a48afc35c18fe15ff0ce38
allis <allis@LAPTOP-H71R1AEC>
2023-12-08 09:33:45 +0900

    2023.12.08 09:33

--- StudyMatch/src/main/java/servlet/LoginAuth.java
+++ StudyMatch/src/main/java/servlet/LoginAuth.java
@@ -13,30 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 
 import member.MemberDAO;
 import member.MemberDTO;
+import utils.JSFunction;
 
-@WebServlet(urlPatterns = "/auth/LoginAuth.do")
+@WebServlet("/auth/LoginAuth.do")
 public class LoginAuth extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		
-		RequestDispatcher dis = req.getRequestDispatcher("Login.jsp");
-		dis.forward(req, resp);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		req.getRequestDispatcher("Login.jsp").forward(req, resp);
 	}
+
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
 		DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
 
-		// 인증 요청한 계정 객체에 저장
 		String id = req.getParameter("id");
 		String pass = req.getParameter("pass");
 		MemberDAO dao = new MemberDAO();
-		MemberDTO dto = dao.getMemberDTO(id, pass);
+		MemberDTO dto = new MemberDTO();
+		dao.login(id, pass);
 		if (dto != null) {
 			req.setAttribute("user", dto);
-			System.out.println("로그인 성공 - 계정 정보 request 저장 완료");
-			resp.sendRedirect(req.getContextPath() + "/auth/MainPage.jsp");
+			System.out.println(date.format(now) + " 로그인 성공 - 계정 정보 request 저장 완료");
+			resp.sendRedirect(req.getContextPath() + "/board/MainPage.jsp");
 		} else {
 			System.out.println("로그인 실패 - 페이지 이동 안 함");
 			resp.sendRedirect("Login.jsp");
