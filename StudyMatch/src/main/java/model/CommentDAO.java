2fb6390444040dd08d61a31545dd0fa354d0e888
heeja <heejaewg@gmail.com>
2023-12-12 17:27:12 +0900

    1212 17:26 댓글 DTO, DAO, 서블릿 생성

---
+++ StudyMatch/src/main/java/model/CommentDAO.java
@@ -0,0 +1,105 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import common.DBConnPool;
+
+public class CommentDAO extends DBConnPool {
+
+	DataSource dataSource;
+	Connection con;
+	Statement stmt;
+	PreparedStatement psmt;
+	ResultSet rs;
+	DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+	LocalDateTime now = LocalDateTime.now();
+
+	public CommentDAO() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/dbcp_myoracle");
+			con = dataSource.getConnection();
+			System.out.println("댓글 DB 연동 성공");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("*** 댓글 DB 연동 중 예외 발생 ***");
+		}
+	}
+
+	// 댓긇 쓰기
+	public int insertComm(CommentDTO dto) {
+		int result = 0;
+		int likeCount = dto.getLike_count() != null ? Integer.parseInt(dto.getLike_count()) : 0;
+		String query = "INSERT INTO comments (board_num, commen_num, content, id, like_count) "
+				+ "VALUES (?, ?, ?, ?, ?)";
+
+		try {
+			con = dataSource.getConnection();
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getBoard_num());
+			psmt.setString(2, dto.getCommen_num());
+			psmt.setString(3, dto.getContent());
+			psmt.setString(4, dto.getId());
+			psmt.setInt(5, likeCount);
+			result = psmt.executeUpdate();
+			System.out.println(date.format(now) + " [ " + dto.getId() + " ] 댓긇 DB 업로드 완료");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("*** 댓글 업데이트 쿼리문 예외 발생 ***");
+			result = 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("*** 댓글 업데이트 중 예외 발생! ***");
+			result = 0;
+		}
+		return result;
+	}
+
+	// 수정하기
+	public int updateComm(CommentDTO dto) {
+		int result = 0;
+		String query = "UPDATE comments SET " + "board_num=?, commen_num=?, content=?";
+
+		try {
+			con = dataSource.getConnection();
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getBoard_num());
+			psmt.setString(2, dto.getCommen_num());
+			psmt.setString(3, dto.getContent());
+			result = psmt.executeUpdate();
+			System.out.println(dto.getBoard_num() + " 번 게시글, " + dto.getCommen_num() + " 번 댓글 수정 완료");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("*** 댓글 수정 중 예외 발생 ***");
+		}
+		return result;
+	}
+	
+	// 삭제하기
+	public void deleteCommen() {
+		CommentDTO dto = new CommentDTO();
+		String query = "DELETE FROM comments WHERE commen_num=?";
+		try {
+			con = dataSource.getConnection();
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getCommen_num());
+			psmt.executeUpdate();
+			System.out.println(dto.getBoard_num() + " 번 게시글 " + dto.getCommen_num() + " 번 댓글 삭제 완료");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("*** 댓글 삭제 중 예외 발생  ***");
+		}
+	}
+	
+}
